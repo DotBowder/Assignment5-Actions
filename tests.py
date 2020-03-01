50156c3d2816a317f688ed4f2a62c294ee1c44fc
Daniel Bowder <d.bowder@gmail.com>
2020-03-01 10:15:16 -0800

    Fixed task.py and test.py. The contents had been swapped.

--- tests.py
+++ tests.py
@@ -1,3 +1,15 @@
+import unittest
+import task
 
-def firstrun():
-        return "success"
+class TestCase(unittest.TestCase):
+
+    def test1(self):
+        expected = "success"
+        self.assertEqual(expected, task.firstrun())
+
+    def test2(self):
+        expected = "failure"
+        self.assertNotEqual(expected, task.firstrun())
+
+if __name__ == '__main__':
+    unittest.main()
