d172a29f7fabb17b4167d1b09697e38de77f2b5c
Daniel Bowder <d.bowder@gmail.com>
2020-03-01 10:50:06 -0800

    Added list function

--- task.py
+++ task.py
@@ -6,6 +6,15 @@ def area(r):
     return 3.14 * r * r
 
 
+def list(l):
+    if (len(l) >= 2):
+        return (l[0], l[len(l) - 1])
+    elif (len(l) == 1):
+        return (l[0], None)
+    else:
+        return (None, None)
+
+
 def date(date1, date2):
     date_diff = abs(date2 - date1)
     return date_diff.days
--- tests.py
+++ tests.py
@@ -20,6 +20,14 @@ class TestCase(unittest.TestCase):
         expected = 3.14 * r * r
         self.assertEqual(expected, task.area(r))
 
+    def list_test1(self):
+        list_obj = ['a', 'b', 'c']
+        expected = ('a', 'c')
+        self.assertEqual(expected, task.list(list_obj))
+        list_obj = []
+        expected = (None, None)
+        self.assertEqual(expected, task.list(list_obj))
+
     def date_test1(self):
         d1 = datetime.datetime(2020, 1, 10)
         d2 = datetime.datetime(1800, 2, 5)
